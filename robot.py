785ace6395aee8bb668df2653acfc27e407e5253
Soudini <arnaud.louys@student-cs.fr>
2020-05-20 15:21:14 +0200

    get _dstance

--- robot.py
+++ robot.py
@@ -1,3 +1,12 @@
+import numpy as np
+
+GRADIENT_STEP = 0.1
 class Robot:
     def __init__(self, pos=[0, 0, 0]):
         self.pos = pos
+
+    def get_distance(self, target):
+        return np.linalg.norm(self.pos[:2] - target.pos[:2])
+
+    def get_potential_direction(self, potential):
+        pass
---
+++ sandbox.py
@@ -0,0 +1,10 @@
+from sheep import Sheep
+from shepherd import Shepherd
+
+import numpy as np
+import time
+
+sheep = Sheep(pos = np.array([1,1,0]))
+shepherd = Shepherd(pos = np.array([0,0,0]))
+
+print(sheep.get_distance(shepherd))
