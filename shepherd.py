eb59ebeab5740504f3ab20a2f492a4d8a7fb7cb3
rengati <remi.deschandol@student-cs.fr>
2020-05-20 15:54:19 +0200

    second potential

--- shepherd.py
+++ shepherd.py
@@ -2,8 +2,10 @@ from robot import Robot
 import numpy as np
 from math import exp
 
-d_star = 1
+d_star = 1  # distance to sheeps
 alpha = exp(d_star)
+
+repulsive_constant = 1  # between shepherds
 
 
 class Shepherd(Robot):
@@ -14,6 +16,13 @@ class Shepherd(Robot):
         """returns dxu for the robot in numpy array formated as [x,y]"""
         potential = self.to_sheeps_potential(np.zeros(2), sheeps)
         return np.array([0, 0])
+
+    def to_shepherd_potential(self, location, shepherds):
+        distances = np.array([np.linalg.norm(location - shepherd.pos[:2]) for shepherd in shepherds])
+        # distances = np.apply_along_axis(lambda sheep: np.linalg.norm(location - sheep.pos[:2]), axis=0, arr=sheeps)
+        potentials = repulsive_constant * np.exp(-distances)
+
+        return np.sum(potentials)
 
     def to_sheeps_potential(self, location, sheeps):
         distances = np.array([np.linalg.norm(location - sheep.pos[:2]) for sheep in sheeps])
