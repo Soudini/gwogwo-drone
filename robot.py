gradient first fix

robot.py
@@ -1,6 +1,8 @@
 import numpy as np
 
-GRADIENT_STEP = 0.1
+GRADIENT_STEP = 0.0001
+
+
 class Robot:
     def __init__(self, pos=[0, 0]):
         self.pos = pos
@@ -10,7 +12,9 @@ class Robot:
 
     def get_potential_direction(self, potential):
         gradient = np.array([
-                potential(self.pos + GRADIENT_STEP * np.array([1,0])) - potential(self.pos),
-                potential(self.pos + GRADIENT_STEP * np.array([0,1])) - potential(self.pos)
-            ])
-        return gradient
+            (potential(self.pos + GRADIENT_STEP * np.array([1, 0])) - potential(self.pos)) / GRADIENT_STEP,
+            (potential(self.pos + GRADIENT_STEP * np.array([0, 1])) - potential(self.pos)) / GRADIENT_STEP
+        ])
+
+        print(gradient)
+        return - gradient

sandbox_shepherd.py
@@ -1,11 +1,66 @@
+import rps.robotarium as robotarium
+from rps.utilities.transformations import *
+from rps.utilities.barrier_certificates import *
+from rps.utilities.misc import *
+from rps.utilities.controllers import *
+
+
 from sheep import Sheep
 from shepherd import Shepherd
 
 import numpy as np
 import time
 
-sheep = Sheep(pos = np.array([1,1,0]))
-sheep_2 = Sheep(pos = np.array([1,2,0]))
-shepherd = Shepherd(pos = np.array([0,0,0]))
+# Instantiate Robotarium object
+N_SHEEP = 1
+N_SHEPHERD = 1
+N = N_SHEEP + N_SHEPHERD
 
-print(shepherd.get_control([sheep, sheep_2], [shepherd]))
+ITERATIONS = 500
+
+sheeps = [Sheep() for _ in range(N_SHEEP)]
+shepherds = [Shepherd() for _ in range(N_SHEPHERD)]
+
+# initial_conditions = np.array(
+#     np.mat('1 0.5 -0.5 0 0.28; 0.8 -0.3 -0.75 0.1 0.34; 0 0 0 0 0'))
+
+initial_conditions = np.array(
+    np.mat('1 0.5; 0.8 -0.3; 0 0'))
+
+
+r = robotarium.Robotarium(number_of_robots=N, show_figure=True,
+                          initial_conditions=initial_conditions, sim_in_real_time=True)
+
+# Create barrier certificates to avoid collision
+uni_barrier_cert = create_unicycle_barrier_certificate()
+
+# define x initially
+x = r.get_poses()
+r.step()
+
+# While the number of robots at the required poses is less
+# than N...
+
+for iteration in range(ITERATIONS):
+
+    # Get poses of agents
+    x = r.get_poses()
+
+    for i in range(x.shape[1]):
+        (sheeps + shepherds)[i].pos = x[:2, i].T
+
+    dxu = np.zeros((2, N))
+    for i in range(x.shape[1]):
+        dxu[:, i] = (sheeps + shepherds)[i].get_control(sheeps, shepherds).T
+
+    # Create safe control inputs (i.e., no collisions)
+    dxu = uni_barrier_cert(dxu, x)
+
+    # Set the velocities
+    r.set_velocities(np.arange(N), dxu)
+
+    # Iterate the simulation
+    r.step()
+
+# Call at end of script to print debug information and for your script to run on the Robotarium server properly
+r.call_at_scripts_end()

shepherd.py
@@ -2,7 +2,7 @@ from robot import Robot
 import numpy as np
 from math import exp
 
-d_star = 1  # distance to sheeps
+d_star = 0.2  # distance to sheeps
 alpha = exp(d_star)
 
 repulsive_constant = 1  # between shepherds
@@ -14,7 +14,8 @@ class Shepherd(Robot):
 
     def get_control(self, sheeps, shepherds):
         """returns dxu for the robot in numpy array formated as [x,y]"""
-        potential = lambda x: self.to_sheeps_potential(x, sheeps) + self.to_shepherd_potential(x, shepherds)
+        # potential = lambda x: self.to_sheeps_potential(x, sheeps) + self.to_shepherd_potential(x, shepherds)
+        potential = lambda x: 10*self.to_sheeps_potential(x, sheeps)
         return self.get_potential_direction(potential)
 
     def to_shepherd_potential(self, location, shepherds):
@@ -28,5 +29,7 @@ class Shepherd(Robot):
         distances = np.array([np.linalg.norm(location - sheep.pos) for sheep in sheeps])
         # distances = np.apply_along_axis(lambda sheep: np.linalg.norm(location - sheep.pos), axis=0, arr=sheeps)
         potentials = distances + alpha * np.exp(-distances)
+
+        print(potentials)
 
         return np.sum(potentials)